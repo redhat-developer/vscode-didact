145161f8086abed3ef0d2e1861be5755ed4920e6
Brian Fitzpatrick <bfitzpat@redhat.com>
2019-10-22 15:59:01 -0600

    adding simple persistence so webview stays open between sessions
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -37,6 +37,15 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(openTutorial);
 
+	if (vscode.window.registerWebviewPanelSerializer) {
+		// Make sure we register a serializer in activation event
+		vscode.window.registerWebviewPanelSerializer(DidactWebviewPanel.viewType, {
+			async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+				console.log(`Got state: ${state}`);
+				DidactWebviewPanel.revive(webviewPanel, context.extensionPath);
+			}
+		});
+	}
 }
 
 function getWebviewContent() {
