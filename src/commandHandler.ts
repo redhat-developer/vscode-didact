3580976aaedb74bc1dc3dc9360ea18bce31eab99
bfitzpat@redhat.com <bfitzpat@redhat.com>
2020-02-05 12:51:58 -0700

    addressing suggestion to simplify code
    Signed-off-by: bfitzpat@redhat.com <bfitzpat@redhat.com>

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -20,7 +20,7 @@ import * as path from 'path';
 import {getValue} from './utils';
 import * as url from 'url';
 import {extensionFunctions} from './extensionFunctions';
-import * as utils from './utils';
+import {getNotificationSetting} from './utils';
 
 // take the incoming didact link and allow a mix of a uri and text/user inputs
 export async function processInputs(incoming : string, extensionPath? : string) : Promise<void | undefined>  {
@@ -218,8 +218,7 @@ async function callCommand(commandId: string, args : any[], completionMessage: s
 				if (completionMessage) {
 					vscode.window.showInformationMessage(completionMessage);
 				} else {
-					let disableDefaultNotifications = utils.getNotificationSetting();
-					if (!disableDefaultNotifications) {
+					if (!getNotificationSetting()) {
 						vscode.window.showInformationMessage(`Didact just executed ${commandId} with arguments ${args}`);
 					}
 				}
