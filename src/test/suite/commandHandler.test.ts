0c4e003a14358c3beced07f86cc79bc3616529fa
bfitzpat@redhat.com <bfitzpat@redhat.com>
2020-08-21 10:16:27 -0600

    adding a test for Beside/Active
    Signed-off-by: bfitzpat@redhat.com <bfitzpat@redhat.com>

--- src/test/suite/commandHandler.test.ts
+++ src/test/suite/commandHandler.test.ts
@@ -17,7 +17,11 @@
 'use strict';
 
 import { expect } from 'chai';
-import { handleNumber } from '../../commandHandler';
+import { handleNumber, processInputs } from '../../commandHandler';
+import { DidactWebviewPanel } from '../../didactWebView';
+import * as vscode from 'vscode';
+import { setLastColumnUsedSetting } from '../../utils';
+
 
 suite("Command Handler tests", function () {
 
@@ -26,6 +30,53 @@ suite("Command Handler tests", function () {
 		handleNumber(output, '2');
 		expect(output[0]).to.be.a('number');
 		expect(output[0]).to.equal(2);
-	})
+	});
 
+	test('try to open a didact file in a different column with startDidact', async function() {
+		const didactUri = 'didact://?commandId=vscode.didact.startDidact&text=https://raw.githubusercontent.com/redhat-developer/vscode-didact/master/examples/copyFileURL.example.didact.md$$Two';
+		await processInputs(didactUri);
+		expect(DidactWebviewPanel.currentPanel).to.not.equal(undefined);
+		if (DidactWebviewPanel.currentPanel) {
+			const column : vscode.ViewColumn | undefined = DidactWebviewPanel.currentPanel.getColumn();
+			expect(column).to.equal(vscode.ViewColumn.Two);
+		}
+		await resetAfterTest();
+	});
+
+	/*
+		Though passing Beside and Active works in practice, when testing, we are not getting back ANY column (undefined).
+		Created https://github.com/microsoft/vscode/issues/105168 
+	*/
+	test.skip('try to open a didact file in the Active column (one) with startDidact', async function() {
+		// reset the column to 1
+		await setLastColumnUsedSetting(1);
+		const didactUri = 'didact://?commandId=vscode.didact.startDidact&text=https://raw.githubusercontent.com/redhat-developer/vscode-didact/master/examples/copyFileURL.example.didact.md$$Active';
+		await processInputs(didactUri);
+		expect(DidactWebviewPanel.currentPanel).to.not.equal(undefined);
+		if (DidactWebviewPanel.currentPanel) {
+			const column : vscode.ViewColumn | undefined = DidactWebviewPanel.currentPanel.getColumn();
+			console.log(`Column returned = ${column}`);
+			expect(column).to.equal(vscode.ViewColumn.One);
+		}
+		await resetAfterTest();
+	});
+
+	test.skip('try to open a didact file in the Beside column (two) with startDidact', async function() {
+		// reset the column to 1
+		await setLastColumnUsedSetting(1);
+		const didactUri = 'didact://?commandId=vscode.didact.startDidact&text=https://raw.githubusercontent.com/redhat-developer/vscode-didact/master/examples/copyFileURL.example.didact.md$$Beside';
+		await processInputs(didactUri);
+		expect(DidactWebviewPanel.currentPanel).to.not.equal(undefined);
+		if (DidactWebviewPanel.currentPanel) {
+			const column : vscode.ViewColumn | undefined = DidactWebviewPanel.currentPanel.getColumn();
+			console.log(`Column returned = ${column}`);
+			expect(column).to.equal(vscode.ViewColumn.Two);
+		}
+		await resetAfterTest();
+	});
 });
+
+async function resetAfterTest() {
+	// reset the column
+	await setLastColumnUsedSetting(1);
+}
