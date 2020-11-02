d59f1156e6010630f9e8b5d2dc1b8fb402f5c0d6
bfitzpat@redhat.com <bfitzpat@redhat.com>
2020-11-02 08:43:44 -0700

    Fixing actual xml validation issues
    Signed-off-by: bfitzpat@redhat.com <bfitzpat@redhat.com>

--- src/didactWebView.ts
+++ src/didactWebView.ts
@@ -316,7 +316,7 @@ export class DidactWebviewPanel {
 			stylesheetHtml = `${adUriHtml}\n ${cssUriHtml}\n`;
 		} else {
 			// use bulma.min.css as the default stylesheet for markdown from https://bulma.io/
-			const bulmaCssHtml = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">`;
+			const bulmaCssHtml = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>`;
 			stylesheetHtml = `${bulmaCssHtml}\n ${cssUriHtml}\n`;
 		}
 
@@ -358,9 +358,9 @@ export class DidactWebviewPanel {
 		const completedHtml = `<!DOCTYPE html>
 		<html lang="en">
 		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self' data: https: http: blob: ${this._panel.webview.cspSource}; media-src vscode-resource: https: data:; script-src 'nonce-${nonce}' https:; style-src 'unsafe-inline' ${this._panel.webview.cspSource} https: data:; font-src ${this._panel.webview.cspSource} https: data:; object-src 'none';">
+			<meta charset="UTF-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self' data: https: http: blob: ${this._panel.webview.cspSource}; media-src vscode-resource: https: data:; script-src 'nonce-${nonce}' https:; style-src 'unsafe-inline' ${this._panel.webview.cspSource} https: data:; font-src ${this._panel.webview.cspSource} https: data:; object-src 'none';"/>
 			<base href="${uriBase}${uriBase.endsWith('/') ? '' : '/'}"/>
 			<title>Didact Tutorial</title>` + 
 			stylesheetHtml + 
@@ -371,6 +371,7 @@ export class DidactWebviewPanel {
 		</body>
 		</html>`;
 
+		console.log(completedHtml);
 		return completedHtml;
 	}
 
@@ -476,16 +477,7 @@ export class DidactWebviewPanel {
 	getFirstHeadingText() : string | undefined {
 		const html : string | undefined = this.getCurrentHTML();
 		if (html) {
-			const parsed = new DOMParser({
-				locator: {},
-				errorHandler: { warning: function () { 
-					// empty 
-				}, 
-				error: function () {
-					// empty
-				}, 
-				fatalError: function (e) { console.error(e) } }
-			}).parseFromString(html);
+			const parsed : Document = new DOMParser().parseFromString(html);
 			if (parsed) {
 				const h1 : HTMLCollectionOf<HTMLHeadingElement> = parsed.getElementsByTagName('h1');
 				if (h1 && h1.length > 0 && h1[0].textContent) {
