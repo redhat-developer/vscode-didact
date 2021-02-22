ebb01e62e2e26b02dc3f2aa8a1828357d7d20cc4
Brian Fitzpatrick <bfitzpat@redhat.com>
2021-02-22 09:08:39 -0700

    FUSETOOLS2-226 - first cut of code coverage
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,17 +1,41 @@
+import * as fs from 'fs';
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import { TestRunnerOptions, CoverageRunner } from './../coverage';
+
+// Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
+// Since we are not running in a tty environment, we just implement the method statically
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tty = require('tty');
+if (!tty.getWindowSize) {
+	tty.getWindowSize = (): number[] => {
+		return [80, 75];
+	};
+}
+
+function loadCoverageRunner(testsRoot: string): CoverageRunner | undefined {
+	let coverageRunner: CoverageRunner;
+	const coverConfigPath = path.join(testsRoot, '..', '..', '..', 'coverconfig.json');
+	if (!process.env.VST_DISABLE_COVERAGE && fs.existsSync(coverConfigPath)) {
+		coverageRunner = new CoverageRunner(JSON.parse(fs.readFileSync(coverConfigPath, 'utf-8')) as TestRunnerOptions, testsRoot);
+		coverageRunner.setupCoverage();
+		return coverageRunner;
+	}
+}
+
+// Create the mocha test
+const mocha = new Mocha({
+	ui: 'tdd',
+	timeout: 20000,
+	reporter: 'mocha-jenkins-reporter',
+	color: true
+});
 
 export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		timeout: 100000,
-		reporter: 'mocha-jenkins-reporter',
-		color: true
-	});
-
 	const testsRoot = path.resolve(__dirname, '..');
+	console.log(`testsRoot = ${testsRoot}`);
+	const coverageRunner = loadCoverageRunner(testsRoot);
 
 	return new Promise((c, e) => {
 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
@@ -30,7 +54,7 @@ export function run(): Promise<void> {
 					} else {
 						c();
 					}
-				});
+				}).on('end', () => coverageRunner && coverageRunner.reportCoverage());
 			} catch (error) {
 				e(error);
 			}
