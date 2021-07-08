43e2d4a3199a3d168d43fbe64b02c067c9a8d2eb
Brian Fitzpatrick <bfitzpat@redhat.com>
2021-07-08 15:11:40 -0600

    removing change to runTest
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
-import * as cp from 'child_process';
-import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from 'vscode-test';
+import { downloadAndUnzipVSCode, runTests } from 'vscode-test';
 
 async function main() {
 
@@ -13,9 +12,6 @@ async function main() {
 
 	const vscodeExecutablePath : string = await downloadAndUnzipVSCode('stable');
 	console.log(`vscodeExecutablePath = ${vscodeExecutablePath}`);
-
-	const cliPath: string = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-	installExtraExtension(cliPath, 'redhat.vscode-commons');
 
 	try {
 		await runTests({ 
@@ -30,14 +26,6 @@ async function main() {
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
-}
-
-function installExtraExtension(cliPath: string, extensionId: string) {
-	cp.spawnSync(cliPath, ['--install-extension', extensionId, '--force'], {
-		encoding: 'utf-8',
-		stdio: 'inherit'
-	});
-	console.log(`VS Code extension ${extensionId} installed`);
 }
 
 main();
